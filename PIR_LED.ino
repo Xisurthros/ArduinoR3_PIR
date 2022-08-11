3515014fea3b62dc8c36b61a6429b580a5b51c9c
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-08-11 13:57:18 -0500

    Update PIR_LED.ino
    Added LED and cleaned up Serial Output

--- PIR_LED.ino
+++ PIR_LED.ino
@@ -2,24 +2,30 @@
 int calibration = 60;
 
 const int PIR = 3;
+const int LED = 8;
 
 void setup() {
   Serial.begin(9600);
   pinMode(PIR, INPUT);
+  pinMode(LED, OUTPUT);
   digitalWrite(PIR, LOW);
 
   //Calibrateion
-  Serial.print("Calibrating Sensor");
+  Serial.println("Calibrating Sensor");
   for (int x = 0; x < calibration; x++){
     Serial.println(x);
     delay(1000);
   }
-  Serial.print("Calibration Complete");
-  Serial.print("Starting");
+  Serial.println("Calibration Complete");
+  Serial.println("Starting");
 
 }
 
 void loop() {
-  Serial.println(digitalRead(PIR));
+  if (digitalRead(PIR) == 1){
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
   delay(100);
 }
